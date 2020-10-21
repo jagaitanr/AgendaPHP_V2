f568531f4d39d973bf86355a0972df31531c05d7
jagaitanr <jagaitanr@hotmail.com>
2020-10-21 16:41:25 -0500

    conexion exitosa

--- codBase/server/check_login.php
+++ codBase/server/check_login.php
@@ -1,5 +1,9 @@
 <?php
-
+require ('./conector.php');
+$con = new ConectorDB();
+$response ['conexion'] = $con->initConexion('agenda2');
+echo json_encode($response);
+//$con->cerrarConexion();
 
 
 
---
+++ codBase/server/conector.php
@@ -0,0 +1,27 @@
+<?php
+
+class ConectorDB
+{
+  private $host = 'localhost';
+  private $user = 'Admin_Agenda2';
+  private $password = '123456';
+  private $conexion;
+
+
+  function initConexion($nombre_db){
+    $this->conexion = new mysqli($this->host, $this->user, $this->password, $nombre_db);
+    if ($this->conexion->connect_error) {
+      return "Error: ".$this->conexion->connect_error;
+    echo "error";
+    }
+    else{
+      return "OK";
+      echo "ok";
+    }
+    $this->close();
+  }
+
+
+  //return $this->ejecutarQuery($sql);
+}
+ ?>
